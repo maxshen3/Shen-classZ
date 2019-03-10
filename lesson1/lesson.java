068bd3fc9084fa59f9161ad7a350e2bfc4d47bee
maxshen3 <maxshen3@gmail.com>
2019-03-10 12:12:17 -0400

    .

---
+++ lesson1/lesson.java
@@ -0,0 +1,39 @@
+/*
+public- everyone can see
+void- makes it go through
+byte code-allows the .class to run through everything
+ln makes the hello go to a different line
+Data types- Primitive - integer=int a = 5 a is the name of the variable, equals is an assignment, 5 is the value 
+Doubles- decimals
+String is NOT primitive, but is very important
+String is an object type and need double quotations
+Character
+boolean- true or false only, no other values
+boolean flag = false
+loops do things very fast
+java has two loops- For loop and while loop 
+for( int i=0; i<10; i++)
+int intialiizes the variabl;e
+condition is the i<10, shows the limitations to the for loop
+*/
+public class lesson{
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		int num=222;
+		System.out.println(num);
+		boolean flag=false;
+		for(int	i=0;i<10;i++){
+
+		} 
+		int y=0;
+		for (int i=10;i<20;i++){
+		
+		}
+		System.out.println("test");
+		for (int i=10;i<20;i++){
+		System.out.println(i);
+
+		}
+		 System.out.println("test");
+	}		
+}
